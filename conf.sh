a71c5dcc478da2e75a56b032184009213ff557e7
duxlong <duxlong@me.com>
2020-08-26 00:00:26 +0800

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-REGION=$1
-AKI=$2
-AKS=$3
-
 # 设置 aliyun-cli 登陆信息
 aliyun configure set \
   --profile akProfile \
@@ -12,10 +8,8 @@ aliyun configure set \
   --access-key-id $AKI \
   --access-key-secret $AKS
 
-DOMAIN=$4
-
 # 设置 crontab
-echo "*/5 * * * * /bin/bash /root/ddns.sh $DOMAIN" > /var/spool/cron/crontabs/root
+echo "*/5 * * * * /bin/bash /root/ddns.sh" > /var/spool/cron/crontabs/root
 
 # 必须 -f 前台运行
 crond -f
