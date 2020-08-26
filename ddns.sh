e5a280c3356a995a09ec4688523d5a8a0b7d9cdf
duxlong <duxlong@me.com>
2020-08-26 23:48:20 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -14,18 +14,17 @@ login(){
 
 # 使用 aliyun-cli 修改域名解析记录
 ddns(){
-    # 分割字符串
-    rr=`echo $DOMAIN | cut -d. -f1`
-
     # 获取子域名的解析记录
     records=$(aliyun alidns DescribeSubDomainRecords --SubDomain $DOMAIN)
+
+    # 分割字符串
+    rr=`echo $DOMAIN | cut -d. -f1`
 
     # 查看解析记录的数量
     count=`echo $records | jq -r '.TotalCount'`
 
     if [ $count -eq 0 ]
     then
-        echo "添加解析记录ing..."
         name=$(echo `echo $DOMAIN | cut -d. -f2`.`echo $DOMAIN | cut -d. -f3`)
         aliyun alidns AddDomainRecord --DomainName $name --RR $rr --Type A --Value $ip
         if [ $? -eq 0 ]
@@ -37,7 +36,6 @@ ddns(){
         fi
         
     else
-        echo "修改解析记录ing..."
         id=`echo $records | jq -r '.DomainRecords.Record | .[-1].RecordId'`
         aliyun alidns UpdateDomainRecord --RecordId $id --RR $rr --Type A --Value $ip
         if [ $? -eq 0 ]
@@ -54,25 +52,29 @@ ddns(){
 
 # 记录阿里云解析的 IP，判断是否成功修改域名解析
 saveIP(){
-    echo $ip > ./aliyunIP.txt
+    echo $ip > ./dnsip.txt
 }
 
 #################### main ####################
 
-# 第一次运行，创建文件并登陆
-if [ ! -f ./aliyunIP.txt ]
+# 第一次运行脚本，登陆 aliyun-cli 并写入 dnsip
+if [ ! -f ./dnsip.txt ]
 then
-    touch ./aliyunIP.txt
+    echo "初始化脚本..."
+    
     login
+
+    # 写入 dnsip
+    records=`aliyun alidns DescribeSubDomainRecords --SubDomain $DOMAIN`
+    dnsip=`echo $records | jq -r '.DomainRecords.Record | .[-1].Value'`
+    echo $dnsip > ./dnsip.txt
 fi
 
-# 获取公网 ip
 ip=`curl -s ip.cip.cc`
 
-# 读取 aliyunIP.txt（上一次登陆阿里云修改的解析记录）
-aliyunIP=`cat ./aliyunIP.txt`
+dnsip=`cat ./dnsip.txt`
 
-if [ $ip = $aliyunIP ]
+if [ $ip = $dnsip ]
 then
     echo "公网 IP 未改变！"
 else
